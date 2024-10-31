7103c6c237134c961937bcdf4d4406f05f287416
Karla Salado <113056744+itzchav@users.noreply.github.com>
2024-10-31 13:29:08 -0600

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/karla/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/karla/DRL-robot-navigation/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/karla/DRL-robot-navigation/catkin_ws/devel;/opt/ros/noetic' != '' else []
